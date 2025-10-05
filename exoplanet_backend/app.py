2c2948d573f1189f330d99b319df1cfe9b5b0e72
Muhammad Waqas <159756301+muhammadwaqas45@users.noreply.github.com>
2025-10-05 17:56:34 +0500

    Update app.py

--- exoplanet_backend/app.py
+++ exoplanet_backend/app.py
@@ -82,8 +82,7 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Railway sets PORT automatically
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
+
 
